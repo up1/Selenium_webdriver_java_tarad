70a72dde15bd840a4531d4a09042c551d7bd6d77
Somkiat Puisungnoen <somkiat.p@gmail.com>
2013-05-06 21:34:16 +0700

    Update sourcecode to add method handleAlert()

--- src/test/java/com/up1/demo/TestTaradLogin.java
+++ src/test/java/com/up1/demo/TestTaradLogin.java
@@ -9,6 +9,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
@@ -18,32 +19,31 @@ public class TestTaradLogin {
 
 	@Before
 	public void init() {
-		File file = new File(
-				"D:\\Somkiat\\research\\test\\selenium\\IEDriverServer_Win32_2.32.3\\IEDriverServer.exe");
+		File file = new File("D:\\Somkiat\\research\\test\\selenium\\IEDriverServer_Win32_2.32.3\\IEDriverServer.exe");
 		System.setProperty("webdriver.ie.driver", file.getAbsolutePath());
 		driver = new InternetExplorerDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
+	private void handleAlert() {
+		try {
+			driver.switchTo().alert().accept();
+		} catch (NoAlertPresentException e) {
+
+		}
+	}
+
 	@Test
 	public void loginSuccess() {
 		driver.get("https://member.tarad.com/");
-		driver.switchTo().alert().accept();
+		handleAlert();
 		driver.findElement(By.id("username")).sendKeys("somkiat_pui");
 		driver.findElement(By.id("password")).sendKeys("42112121");
 		driver.findElement(By.className("table_normal")).submit();
-		driver.switchTo().alert().accept();
-		// (new WebDriverWait(driver, 10)).until(new
-		// ExpectedCondition<Boolean>() {
-		// public Boolean apply(WebDriver d) {
-		// return d.getTitle().toLowerCase().startsWith("จัดการข้อมูลสมาชิก");
-		// }
-		// });
+		handleAlert();
 
-		assertEquals(driver.getTitle(),
-				"จัดการข้อมูลสมาชิก - ระบบสมาชิกตลาดดอทคอม");
-		assertEquals(driver.findElement(By.className("text-red-rakuten"))
-				.getText(), "somkiat_pui");
+		assertEquals(driver.getTitle(), "จัดการข้อมูลสมาชิก - ระบบสมาชิกตลาดดอทคอม");
+		assertEquals(driver.findElement(By.className("text-red-rakuten")).getText(), "somkiat_pui");
 
 		driver.findElement(By.linkText("ออกจากระบบ")).click();
 		driver.switchTo().alert().accept();
@@ -53,51 +53,44 @@ public class TestTaradLogin {
 	@Test
 	public void loginFailureWithWrongPassword() {
 		driver.get("https://member.tarad.com/");
-		driver.switchTo().alert().accept();
+		handleAlert();
 		driver.findElement(By.id("username")).sendKeys("somkiat_pui");
 		driver.findElement(By.id("password")).sendKeys("xxxxx");
 		driver.findElement(By.className("table_normal")).submit();
-		driver.switchTo().alert().accept();
-
+		handleAlert();
 		assertEquals(driver.getTitle(), "เข้าสู่ระบบ - ระบบสมาชิกตลาดดอทคอม");
-		assertEquals(driver.findElement(By.className("box-return-message"))
-				.getText(),
-				"รหัสผ่านไม่ถูกต้อง กรุณากรอกรหัสผ่านใหม่");
+		assertEquals(driver.findElement(By.className("box-return-message")).getText(), "รหัสผ่านไม่ถูกต้อง กรุณากรอกรหัสผ่านใหม่");
 	}
-	
+
 	@Test
 	public void loginFailureWithInvalidPassword() {
 		driver.get("https://member.tarad.com/");
-		driver.switchTo().alert().accept();
+		handleAlert();
 		driver.findElement(By.id("username")).sendKeys("somkiat_pui");
 		driver.findElement(By.id("password")).sendKeys("xxx");
 		driver.findElement(By.className("table_normal")).submit();
-		driver.switchTo().alert().accept();
+		handleAlert();
 
 		assertEquals(driver.getTitle(), "เข้าสู่ระบบ - ระบบสมาชิกตลาดดอทคอม");
-		assertEquals(driver.findElement(By.className("box-return-message"))
-				.getText(),
-				"กรุณากรอกข้อมูลให้ครบ");
+		assertEquals(driver.findElement(By.className("box-return-message")).getText(), "กรุณากรอกข้อมูลให้ครบ");
 	}
-	
+
 	@Test
 	public void loginFailureWithUserNotFound() {
 		driver.get("https://member.tarad.com/");
-		driver.switchTo().alert().accept();
+		handleAlert();
 		driver.findElement(By.id("username")).sendKeys("somkiat_pui1234");
 		driver.findElement(By.id("password")).sendKeys("xxxxx");
 		driver.findElement(By.className("table_normal")).submit();
-		driver.switchTo().alert().accept();
+		handleAlert();
 
 		assertEquals(driver.getTitle(), "เข้าสู่ระบบ - ระบบสมาชิกตลาดดอทคอม");
-		assertEquals(driver.findElement(By.className("box-return-message"))
-				.getText(),
-				"ไม่มีชื่อล๊อกอินนี้อยู่ในระบบ กรุณากรอกชื่อล๊อกอินใหม่");
+		assertEquals(driver.findElement(By.className("box-return-message")).getText(), "ไม่มีชื่อล๊อกอินนี้อยู่ในระบบ กรุณากรอกชื่อล๊อกอินใหม่");
 	}
 
 	@After
 	public void destroy() {
-		driver.close();
+		driver.quit();
 	}
 
 }
